2f9f75bd2caacbf7d1a096530a66774ef39e62f9
Ahnm1 <hansson.alexander@live.se>
2023-02-07 20:00:12 +0100

    Cleaning and updating

--- tests/smhi_plot.py
+++ tests/smhi_plot.py
@@ -1,47 +1,48 @@
 #%%
 import  os
-import plotly.express as px
-import pandas as pd
-import numpy as np
-
-#%%
-clean_list = os.listdir('clean')
-
-df = pd.read_csv('clean/' + clean_list[0], index_col = 'Datum')
-df1 = pd.read_csv('clean/' + clean_list[1], index_col = 'Datum')
-df.tail(30)
-#%%
-# df = pd.DataFrame(np.random.random((200,3)))
-# df['Datum'] = pd.date_range('2021-11-1', periods=365*2, freq='D')
-# df = df.set_index(['Datum'])
-df.loc['2021-11-1':'2022-10-31']
-
-#%%
-dfc = df.loc['2021-11-1':'2022-10-31']
-dfc1 = df1.loc['2021-11-1':'2022-10-31']
-
-
-#%%
 import plotly.graph_objects as go
+import pandas as pd
 
-df1 = dfc.reset_index()
-df2 = dfc1.reset_index()
+#%%
+final_path = 'data/final/'
+clean_list = os.listdir('data/final')
+
+df = pd.read_csv(final_path + clean_list[0], index_col = 'timestamp')
+df1 = pd.read_csv(final_path + clean_list[1], index_col = 'timestamp')
+df.tail(30)
+
+#%%
+
+
+df1 = df.reset_index()
+# df2 = dfc1.reset_index()
 
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(x = df1['Datum'],
-    y    = df1['Lufttemperatur'],
-    name = '',
-    line = dict(color='firebrick',
-    width = 4)))
+fig.add_trace(go.Scatter(x = df1['timestamp'],
+    y    = df1['wind_avg'],
+    name = 'temp',
+    line = dict(
+        color = 'firebrick',
+        width = 2,
+        )
+    )
+)
 
-fig.add_trace(go.Scatter(x = df2['Datum'],
-    y    = df1['Lufttemperatur'],
-    name = '',
-    line = dict(color='royalblue',
-    width = 4)))
+fig.add_trace(go.Scatter(x = df1['timestamp'],
+    y    = df1['temperature'],
+    name = 'wind',
+    line = dict(
+        color = 'royalblue',
+        width = 2
+        )
+    )
+)
 
-fig.update_layout(title='Temperatures',
+fig.update_layout(title='Temperature & Wind',
                    xaxis_title='Month',
-                   yaxis_title='Temperature (degrees C)')
+                   yaxis_title='Value'
+                )
 
+
+# %%
