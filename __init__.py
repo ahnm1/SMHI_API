5ad4aac737b41114ff26c32cd1826c0f81d725e4
Ahnm1 <hansson.alexander@live.se>
2023-02-07 16:48:54 +0100

    implement merging

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@
 import os
 import pandas as pd
 from HandleSMHI import HandleSMHI
+from MergerSMHI import MergerSMHI
 
 def get_raw(parameter = 'temperature'):
     '''Works only with "temperature" and "wind" parameters'''
-
+    
+    handler       = HandleSMHI()
     weather_value = 0
 
     if parameter == 'wind':
@@ -14,9 +16,8 @@ def get_raw(parameter = 'temperature'):
     elif parameter == 'temperature':
         weather_value = 1
 
-    handler = HandleSMHI()
-
     with open('data/station_keys.csv', 'r', encoding = 'utf-8-sig') as station_keys:
+
         for line in station_keys.readlines()[1:]:
             city    = line.split(',')[0].capitalize().replace(' ', '_')
             city_id = line.split(',')[1].replace('\n', '')
@@ -35,19 +36,23 @@ def clean_raw():
         handler.clean_csv(file)
 
 
-
 def save_samples(start_date, end_date,):
     handler = HandleSMHI()
 
     clean_list = os.listdir('data/clean')
 
     for file in clean_list:
-        print(file)
         handler.get_data_between(start_date, end_date, file)
 
+
+def merge_samples():
+    targets = os.listdir('data/target')
+    merger  = MergerSMHI()
+    merger.merge_dfs(targets)
 
 if __name__ == '__main__':
     # get_raw('wind')
     # clean_raw()
     # save_samples('2021-11-01', '2022-10-31')
+    merge_samples()
     pass
