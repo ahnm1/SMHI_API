8b75f92b40fc0e61f4396020fd21c4e6757d6d4a
Ahnm1 <hansson.alexander@live.se>
2023-02-07 13:30:46 +0100

    Fully working __init__

---
+++ __init__.py
@@ -0,0 +1,53 @@
+
+import os
+import pandas as pd
+from HandleSMHI import HandleSMHI
+
+def get_raw(parameter = 'temperature'):
+    '''Works only with "temperature" and "wind" parameters'''
+
+    weather_value = 0
+
+    if parameter == 'wind':
+        weather_value = 25
+
+    elif parameter == 'temperature':
+        weather_value = 1
+
+    handler = HandleSMHI()
+
+    with open('data/station_keys.csv', 'r', encoding = 'utf-8-sig') as station_keys:
+        for line in station_keys.readlines()[1:]:
+            city    = line.split(',')[0].capitalize().replace(' ', '_')
+            city_id = line.split(',')[1].replace('\n', '')
+
+            print(f" Procesing: { city }\n\tID: { city_id }")
+
+            handler.get_raw_csv(name = city, station_id = city_id, parameter = weather_value)
+
+
+def clean_raw():
+    raw_list = os.listdir('data/raw')
+    handler  = HandleSMHI()
+
+    for file in raw_list:
+        print('data/raw/' + file)
+        handler.clean_csv(file)
+
+
+
+def save_samples(start_date, end_date,):
+    handler = HandleSMHI()
+
+    clean_list = os.listdir('data/clean')
+
+    for file in clean_list:
+        print(file)
+        handler.get_data_between(start_date, end_date, file)
+
+
+if __name__ == '__main__':
+    # get_raw('wind')
+    # clean_raw()
+    # save_samples('2021-11-01', '2022-10-31')
+    pass
