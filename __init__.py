aef7ff018e3942732ef9916b80b88bfec17e262c
Ahnm1 <hansson.alexander@live.se>
2023-02-08 15:42:15 +0100

    added comment

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 from MainSMHI import MainSMHI 
 
-
 if __name__ == '__main__':
     smhi = MainSMHI()
-    smhi.get_raw()
+    smhi.get_raw() # for temperature
     smhi.get_raw('wind')
     smhi.clean_raw()
     smhi.save_samples('2021-11-01', '2022-10-31')
